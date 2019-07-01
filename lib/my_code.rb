f0487788f1e73bb6b0122c5ece7e2bb49eed8473
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-01 15:15:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,5 +4,5 @@ end
 
 def reduce(element1, starting_point = 0, &block)
   testReduce = element1.reduce(starting_point, &block)
-  testReduce.reduce { |x, y| if x || y = true; p true; else p false; end}
+  testReduce.reduce({ |x, y| if x || y = true; p true; else p false; end})
   end
