56dce1dbc1a4e8874dbbe372eb31d78dfc7dabb1
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-01 14:56:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,6 @@ def map(element1, &block)
   element1.map(&block)
 end
 
-def reduce(element1, starting_point =, &block)
+def reduce(element1, starting_point = 0, &block)
   element1.reduce(&block)
 end
