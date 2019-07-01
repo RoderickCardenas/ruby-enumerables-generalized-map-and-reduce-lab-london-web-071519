85f56d456e3468b4d3b6e50188f5f2596b7052c9
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-01 15:00:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,5 +3,5 @@ def map(element1, &block)
 end
 
 def reduce(element1, starting_point = 0, &block)
-  element1.reduce(starting_point, &block) { |x, y| if x && y = true; p true; else p false;}
+  element1.reduce(starting_point, &block) { |x, y| if x || y = true; p true; else p false;}
 end
