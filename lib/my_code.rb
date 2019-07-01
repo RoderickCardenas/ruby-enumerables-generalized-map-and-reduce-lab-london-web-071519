ebc1b4d8f87fdc0d40500fc60a577e1c6caf12d1
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-01 13:39:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,4 @@
 def map(element1) 
-  newArray = []
-  newArray.push(element1)
+  newArray = newArray.inject(element1) { |a,element| a << element.dup }
   negativize = element1.map { |n| n * -1 }
 end
