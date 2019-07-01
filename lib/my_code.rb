bdb41a02ede313af76a069a42fc650d188e5d88f
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-01 12:54:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,7 @@
-# Your Code Here
+def make_sandwich(element1, element2)
+  yield("A #{element1} and #{element2} sandwich")
+end
+
+make_sandwich("Creamy peanut butter", "glittering sense of acccomplishment") {
+  |contents| "#{contents} on wheat"
+}
